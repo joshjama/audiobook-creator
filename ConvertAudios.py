b8865f010b475c6529f3f2ebf953dc3cedaf9475
joshjama <joshjama@gmail.com>
2024-08-04 19:19:21 +0200

    Added monitoring cpu temperature tothe mp3 - converter.

--- ConvertAudios.py
+++ ConvertAudios.py
@@ -1,11 +1,14 @@
 import os
 import zipfile
 import sys
+import time 
+import psutil 
 from pydub import AudioSegment
 
 def convert_audios(folder):
     os.chdir(folder)
     for file in os.listdir():
+        monitor_cpu_temp() 
         if file.endswith(".wav"):
             print("Converting:", file)
             input_file = file
@@ -15,6 +18,7 @@ def convert_audios(folder):
     
     os.makedirs("mp3", exist_ok=True)
     for file in os.listdir():
+        monitor_cpu_temp()
         if file.endswith(".mp3"):
             os.rename(file, os.path.join("mp3", file))
 
@@ -37,13 +41,36 @@ def zip_mp3_files(folder, book_name):
     zip_filename = book_name + '.zip'
     with zipfile.ZipFile(zip_filename, "w") as zip_file:
         for file in os.listdir(mp3_folder):
-            if file.endswith(".mp3"):
-                file_path = os.path.join(mp3_folder, file)
-                zip_file.write(file_path, os.path.basename(file_path))
-                #zip_file.write(os.path.join(mp3_folder, file))
+           monitor_cpu_temp()
+           if file.endswith(".mp3"):
+              file_path = os.path.join(mp3_folder, file)
+              zip_file.write(file_path, os.path.basename(file_path))
+              #zip_file.write(os.path.join(mp3_folder, file))
     
     os.chdir(script_directory)
     print(f"ZIP file '{zip_filename}' created successfully.")
+
+def get_cpu_temp():
+    try:
+        temp = psutil.sensors_temperatures(fahrenheit=False)['coretemp'][0].current
+        return temp
+    except (KeyError, AttributeError):
+        print("Error: Unable to read CPU temperature.")
+        return None
+
+def monitor_cpu_temp():
+    temp = get_cpu_temp()
+    temp_as_int = int(float(temp)) 
+    if temp is not None:
+        print(f"Current CPU temperature: {temp:.2f}°C")
+        if temp_as_int >= 80:
+            print("Warning: CPU temperature exceeds 80°C. Introducing a 10-second pause.")
+            time.sleep(10)
+    else:
+        print("Unable to read CPU temperature. Retrying in 5 seconds.")
+        time.sleep(10)
+
+
 
 
 if __name__ == "__main__":
